0b4b775a154e8653afffcf00a3a3f11e747027ea
Maki Hirose <Capture@Makis-MBP-RETINA-13.local>
2019-04-21 15:34:13 -0400

    Schema for JawsDB created

---
+++ db/schemaForJawsDB.sql
@@ -0,0 +1,10 @@
+### Schema for JawsDB
+
+CREATE TABLE burgers
+(
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(100) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
